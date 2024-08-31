58530761a83466c7d818b8ec8e2493f84d3f1b06
SntLF02 <20santi.lopez02@gmail.com>
2024-08-31 01:46:30 -0300

    ej11,12,13

---
+++ tp2 programacion python/ejerc11.py
@@ -0,0 +1,8 @@
+#- Pedir dos palabras por teclado, indicar si son iguales.
+palabra1=str(input("Ingrese una palabra: "))
+palabra2=str(input("Ingrese otra palabra: "))
+
+if palabra1==palabra2:
+    print("Las palabras ingresadas son iguales")
+else:
+    print("Las palabras ingresadas no son iguales")
---
+++ tp2 programacion python/ejerc12.py
@@ -0,0 +1,9 @@
+# Dada la cadena “hipopotamo”, extraer la cuarta y quinta letra. 
+cadena=str("Hipopotamo")
+cadena=list(cadena)
+
+print(f"La cuarta y quinta letra de la palabra 'hipopotamo' son: {cadena[3]} y {cadena[4]}")
+
+for i in range (0,2):
+    print("hola")
+    print (i)
